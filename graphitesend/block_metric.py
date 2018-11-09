c820795e281478771878317af7ac93a1505f1a8f
Alexandre Bonnetain <shir0kamii@gmail.com>
2018-11-09 14:15:01 +0000

    Refactor block metric to use one contextmanager per metric

--- graphitesend/block_metric.py
+++ graphitesend/block_metric.py
@@ -1,19 +1,43 @@
+from contextlib import contextmanager, ExitStack
 from time import time
 
 
+@contextmanager
+def executions(client, metric):
+    try:
+        yield
+    finally:
+        client.send(metric + ".executions", 1)
+
+
+@contextmanager
+def errors(client, metric):
+    try:
+        yield
+    except Exception:
+        client.send(metric + ".errors", 1)
+
+
+@contextmanager
+def processing_time(client, metric):
+    start = time()
+    try:
+        yield
+    finally:
+        client.send(metric + ".processing_time", time() - start)
+
+
 class BlockMetric:
+    trackers = [executions, errors, processing_time]
 
     def __init__(self, client, metric):
         self.client = client
         self.metric = metric
 
     def __enter__(self):
-        self.start = time()
+        self.stack = ExitStack()
+        for tracker in self.trackers:
+            self.stack.enter_context(tracker(self.client, self.metric))
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.client.send(self.metric + ".executions", 1)
-        if exc_type is not None:
-            self.client.send(self.metric + ".errors", 1)
-            return
-        processing_time = time() - self.start
-        self.client.send(self.metric + ".processing_time", processing_time)
+        self.stack.__exit__(exc_type, exc_val, exc_tb)
